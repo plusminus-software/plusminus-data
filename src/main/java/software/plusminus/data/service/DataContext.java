f40c67c4d09bb3c0c43010269ffca0cc06617249
Taras Shpek <taras.shpek@gmail.com>
2025-12-03 16:51:08 +0200

    Replace constructor with init() method in DataContext

--- src/main/java/software/plusminus/data/service/DataContext.java
+++ src/main/java/software/plusminus/data/service/DataContext.java
@@ -1,5 +1,7 @@
 package software.plusminus.data.service;
 
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Component;
 import software.plusminus.data.exception.DataException;
@@ -9,8 +11,8 @@ import software.plusminus.data.repository.DataRepository;
 import software.plusminus.data.repository.SpringCrudRepository;
 import software.plusminus.spring.SpringUtil;
 
-import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 import javax.annotation.Nullable;
 
 @SuppressWarnings("java:S119")
@@ -19,18 +21,22 @@ public class DataContext {
 
     private Map<Class<?>, CrudService<?, ?>> crudServices;
     private Map<Class<?>, CrudRepository<?, ?>> crudRepositories;
-    private @Nullable DataRepository dataRepository;
+    @Nullable
+    private DataRepository dataRepository;
 
-    public DataContext(List<CrudService<?, ?>> crudServices,
-                       List<CrudRepository<?, ?>> crudRepositories,
-                       List<PagingAndSortingRepository<?, ?>> pagingAndSortingRepositories,
-                       @Nullable DataRepository dataRepository) {
-        this.crudServices = SpringUtil.beansToMapByGenericType(crudServices, CrudService.class);
-        this.crudRepositories = SpringUtil.beansToConcurrentMapByGenericType(crudRepositories, CrudRepository.class);
+    @Autowired
+    void init(ObjectProvider<CrudService<?, ?>> crudServices,
+              ObjectProvider<CrudRepository<?, ?>> crudRepositories,
+              ObjectProvider<PagingAndSortingRepository<?, ?>> pagingAndSortingRepositories,
+              @Nullable DataRepository dataRepository) {
+        this.crudServices = SpringUtil.beansToMapByGenericType(crudServices.stream()
+                .collect(Collectors.toList()), CrudService.class);
+        this.crudRepositories = SpringUtil.beansToConcurrentMapByGenericType(crudRepositories.stream()
+                .collect(Collectors.toList()), CrudRepository.class);
+        this.dataRepository = dataRepository;
         pagingAndSortingRepositories.forEach(repository -> this.crudRepositories.computeIfAbsent(
                 SpringUtil.resolveGenericType(repository, PagingAndSortingRepository.class),
                 type -> new SpringCrudRepository<>(repository)));
-        this.dataRepository = dataRepository;
     }
 
     @Nullable
